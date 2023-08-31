7e27259d3893182ad6a68ade2d6a4d0c81178906
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-31 17:07:43 -0300

    perf: mudança de loc pra at

--- gerar_tabela.py
+++ gerar_tabela.py
@@ -42,32 +42,32 @@ def preencher_tabela(tabela_transicao):
     tabela_transicao.at['q0', '\t'] = 'q0'
     tabela_transicao.at['q0', '\n'] = 'q0'
     tabela_transicao.at['q0', ' '] = 'q0'
-    tabela_transicao.loc["q0", '"']= 'q1'
+    tabela_transicao.at["q0", '"']= 'q1'
     tabela_transicao.loc["q0", "a": "z"]= 'q2'
     tabela_transicao.loc["q0", 'A':'F']= 'q9'
     tabela_transicao.loc["q0", 'G':'Z']= 'q5'
     tabela_transicao.loc["q0", '0':'9']= 'q9'
-    tabela_transicao.loc["q0", '|']= 'TK_OP_OU'
-    tabela_transicao.loc["q0", '&']= 'TK_OP_E'
-    tabela_transicao.loc["q0", '~']= 'TK_OP_NEGACAO'
-    tabela_transicao.loc["q0", '(']= 'TK_ABRE_PARENTESE'
-    tabela_transicao.loc["q0", ')']= 'TK_FECHA_PARENTESE'
-    tabela_transicao.loc["q0", ',']= 'TK_VIRGULA'
-    tabela_transicao.loc["q0", '+']= 'TK_OP_SOMA'
-    tabela_transicao.loc["q0", '-']= 'TK_OP_SUB'
-    tabela_transicao.loc["q0", '*']= 'TK_OP_MULT'
-    tabela_transicao.loc["q0", '/']= 'TK_OP_DIV'
-    tabela_transicao.loc["q0", '=']= 'TK_OP_IGUAL'
-    tabela_transicao.loc["q0", ':']= 'q15'
-    tabela_transicao.loc["q0", '!']= 'q16'
-    tabela_transicao.loc["q0", '<']= 'q23'
-    tabela_transicao.loc["q0", '>']= 'q4'
-    tabela_transicao.loc["q0", '#']= 'q22'
-    tabela_transicao.loc["q0", "'"]= 'q17'
+    tabela_transicao.at["q0", '|']= 'TK_OP_OU'
+    tabela_transicao.at["q0", '&']= 'TK_OP_E'
+    tabela_transicao.at["q0", '~']= 'TK_OP_NEGACAO'
+    tabela_transicao.at["q0", '(']= 'TK_ABRE_PARENTESE'
+    tabela_transicao.at["q0", ')']= 'TK_FECHA_PARENTESE'
+    tabela_transicao.at["q0", ',']= 'TK_VIRGULA'
+    tabela_transicao.at["q0", '+']= 'TK_OP_SOMA'
+    tabela_transicao.at["q0", '-']= 'TK_OP_SUB'
+    tabela_transicao.at["q0", '*']= 'TK_OP_MULT'
+    tabela_transicao.at["q0", '/']= 'TK_OP_DIV'
+    tabela_transicao.at["q0", '=']= 'TK_OP_IGUAL'
+    tabela_transicao.at["q0", ':']= 'q15'
+    tabela_transicao.at["q0", '!']= 'q16'
+    tabela_transicao.at["q0", '<']= 'q23'
+    tabela_transicao.at["q0", '>']= 'q4'
+    tabela_transicao.at["q0", '#']= 'q22'
+    tabela_transicao.at["q0", "'"]= 'q17'
     #q1
     tabela_transicao.loc["q1", :]= 'q1'
-    tabela_transicao.loc["q1", '"']= 'TK_CADEIA'
-    tabela_transicao.loc["q1", '\n']= pd.NaN
+    tabela_transicao.at["q1", '"']= 'TK_CADEIA'
+    tabela_transicao.at["q1", '\n']= pd.NaN
     #q2
     tabela_transicao.loc["q2", 'a':'z']= 'q3'
     #q3
@@ -76,12 +76,12 @@ def preencher_tabela(tabela_transicao):
     tabela_transicao.loc["q3", '0':'outro']= 'TK_RESERVADA'
     #q4
     tabela_transicao.loc["q4", :]= 'TK_OP_MAIOR'
-    tabela_transicao.loc["q4", '=']= 'TK_OP_GE'
+    tabela_transicao.at["q4", '=']= 'TK_OP_GE'
     #q5
-    tabela_transicao.loc["q5", '$']= 'q6'
+    tabela_transicao.at["q5", '$']= 'q6'
     #q6
     tabela_transicao.loc["q6", '0':'9']= 'q6'
-    tabela_transicao.loc["q6", '.']= 'q7'
+    tabela_transicao.at["q6", '.']= 'q7'
     #q7
     tabela_transicao.loc["q7", '0':'9']= 'q8'
     #q8
@@ -89,13 +89,13 @@ def preencher_tabela(tabela_transicao):
     #q9
     tabela_transicao.loc["q9", '0':'9']= 'q9'
     tabela_transicao.loc["q9", 'A':'F']= 'q9'
-    tabela_transicao.loc["q9", '$']= 'q6'
-    tabela_transicao.loc["q9", 'e']= 'q10'
-    tabela_transicao.loc["q9", '.']= 'q13'
+    tabela_transicao.at["q9", '$']= 'q6'
+    tabela_transicao.at["q9", 'e']= 'q10'
+    tabela_transicao.at["q9", '.']= 'q13'
     #q10
     tabela_transicao.loc["q10", '0':'9']= 'q12'
     tabela_transicao.loc["q10", 'A':'F']= 'q12'
-    tabela_transicao.loc["q10", '-']= 'q11'
+    tabela_transicao.at["q10", '-']= 'q11'
     #q11
     tabela_transicao.loc["q11", '0':'9']= 'q12'
     tabela_transicao.loc["q11", 'A':'F']= 'q12'
@@ -103,7 +103,7 @@ def preencher_tabela(tabela_transicao):
     tabela_transicao.loc["q12", '0':'9']= 'q12'
     tabela_transicao.loc["q12", 'A':'F']= 'q12'
     tabela_transicao.loc["q12", 'G':'z']= 'TK_NUMERO'
-    tabela_transicao.loc["q12", '(':]= 'TK_NUMERO'
+    tabela_transicao.at["q12", '(':]= 'TK_NUMERO'
     #q13
     tabela_transicao.loc["q13", '0':'9']= 'q14'
     tabela_transicao.loc["q13", 'A':'F']= 'q14'
@@ -111,36 +111,36 @@ def preencher_tabela(tabela_transicao):
     tabela_transicao.loc["q14", '0':'9']= 'q14'
     tabela_transicao.loc["q14", 'A':'F']= 'q14'
     tabela_transicao.loc["q14", 'G':'z']= 'TK_NUMERO'
-    tabela_transicao.loc["q14", '(':]= 'TK_NUMERO'
-    tabela_transicao.loc["q14", 'e']= 'q10'
+    tabela_transicao.at["q14", '(':]= 'TK_NUMERO'
+    tabela_transicao.at["q14", 'e']= 'q10'
     #q15
-    tabela_transicao.loc["q15", '='] = "TK_OP_ATRIBUICAO"
+    tabela_transicao.at["q15", '='] = "TK_OP_ATRIBUICAO"
     #q16
-    tabela_transicao.loc["q16", '='] = "TK_OP_DIF"
+    tabela_transicao.at["q16", '='] = "TK_OP_DIF"
     #q17
-    tabela_transicao.loc["q17", "'"] = "q18"
+    tabela_transicao.at["q17", "'"] = "q18"
     #q18
-    tabela_transicao.loc["q18", "'"] = "q19"
+    tabela_transicao.at["q18", "'"] = "q19"
     #q19
-    tabela_transicao.loc["q19", :] = "q19"
+    tabela_transicao.at["q19", :] = "q19"
     tabela_transicao.at["q19", "'"] = "q20"
     tabela_transicao.at["q19", "\n"] = "q25"
     #q20
     tabela_transicao.loc["q20", :] = "q19"
-    tabela_transicao.loc["q20", "'"] = "q21"
+    tabela_transicao.at["q20", "'"] = "q21"
     #q21
     tabela_transicao.loc["q21", :] = "q19"
-    tabela_transicao.loc["q21", "'"] = "TK_COMENTARIO"
+    tabela_transicao.at["q21", "'"] = "TK_COMENTARIO"
     #q22
     tabela_transicao.loc["q22", :] = "q22"
-    tabela_transicao.loc["q22", "\n"] = "TK_COMENTARIO"
+    tabela_transicao.at["q22", "\n"] = "TK_COMENTARIO"
     #q23
     tabela_transicao.loc["q23", :] = "TK_OP_MENOR"
     tabela_transicao.loc["q23", 'a':'z'] = "q24"
-    tabela_transicao.loc["q23", '='] = "TK_OP_LE"
+    tabela_transicao.at["q23", '='] = "TK_OP_LE"
     #q24
     tabela_transicao.loc["q24", "a":"9"] = "q24"
-    tabela_transicao.loc["q24", ">"] = "TK_ID"
+    tabela_transicao.at["q24", ">"] = "TK_ID"
     #q25
     tabela_transicao.loc["q25", :] = "q19"
     tabela_transicao.at["q25", "'"] = "q20"
