d2ddc8fba16faa2eb189cede3d80b01552a97ad6
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-31 18:12:37 -0300

    feat: def scanner

--- scanner.py
+++ scanner.py
@@ -10,27 +10,33 @@ def prox_estado(estado_atual, char):
     return tabela_transicao.at[estado_atual, char]
 
 def gerar_tbl_simb():
-    cols = ['linha', 'coluna', 'lexema', 'token']
+    cols = ['posicao', 'linha', 'coluna', 'lexema', 'token']
     tbl_simb = pd.DataFrame(columns=cols)
     return tbl_simb
 
 def scanner(nome_arq):
     tbl_transicao, estados_acc = gt.gerar_tabela()
     tbl_simbolos = gerar_tbl_simb()
-    count_linha = 1
     posicao = 0
     
+    arquivo = open(nome_arq, 'r')
+    
     while(1):
-        estado_atual = 'q0'
         lexema = ''
+        token, lexema, posicao = get_token(arquivo, posicao, tbl_transicao, estados_acc)
+        if(token == 'EOF'):
+            break
         if(token != 'TK_COMENTARIO'):
-            add_token(tbl_simbolos, linha, coluna, lexema, token)
+            add_token(tbl_simbolos, posicao, lexema, token)
+    arquivo.close()
     return tbl_simbolos
 
-def add_token(tabela, linha, coluna, lexema, token):
+def add_token(tabela, posicao, lexema, token):
     distintos = ["TK_ID", "TK_NUMERO", "TK_CADEIA", "TK_MOEDA"]
     if(token in distintos):
-        tabela.loc[len(tabela)] = [linha, coluna, lexema, token]
+        tabela.loc[len(tabela)] = [posicao, None, None, lexema, token]
     else:
-        tabela.loc[len(tabela)] = [linha, coluna, None , token]
+        tabela.loc[len(tabela)] = [posicao, None, None, None , token]
     return tabela
+
+    
