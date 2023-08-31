bc083e17bbc05edea1e3bc8f715f35ed8e49459b
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-31 17:49:19 -0300

    feat: gerar tbl_simbolos e add_tokens

--- scanner.py
+++ scanner.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import gerar_tabela as gt
 
-tabela_transicao, estados_acc = gt.gerar_tabela()
-print(estados_acc)
 
 
 # lista com palavras reservadas da linguagem
@@ -10,3 +8,29 @@ reserv = ["programa", "fim_programa", "se", "entao", "imprima", "leia", "enquant
 
 def prox_estado(estado_atual, char):
     return tabela_transicao.at[estado_atual, char]
+
+def gerar_tbl_simb():
+    cols = ['linha', 'coluna', 'lexema', 'token']
+    tbl_simb = pd.DataFrame(columns=cols)
+    return tbl_simb
+
+def scanner(nome_arq):
+    tbl_transicao, estados_acc = gt.gerar_tabela()
+    tbl_simbolos = gerar_tbl_simb()
+    count_linha = 1
+    posicao = 0
+    
+    while(1):
+        estado_atual = 'q0'
+        lexema = ''
+        if(token != 'TK_COMENTARIO'):
+            add_token(tbl_simbolos, linha, coluna, lexema, token)
+    return tbl_simbolos
+
+def add_token(tabela, linha, coluna, lexema, token):
+    distintos = ["TK_ID", "TK_NUMERO", "TK_CADEIA", "TK_MOEDA"]
+    if(token in distintos):
+        tabela.loc[len(tabela)] = [linha, coluna, lexema, token]
+    else:
+        tabela.loc[len(tabela)] = [linha, coluna, None , token]
+    return tabela
