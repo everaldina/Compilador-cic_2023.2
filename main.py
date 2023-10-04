1e53e5c0d06ff6cac29523a7653825c7ccb04a5e
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-03 21:35:23 -0300

    add saida de tokens

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pandas as pd
 def main(nome_arquivo):
     tbl_simbolos, tbl_erros, cursor = scanner.scanner(nome_arquivo)
     count_linha = cursor["linha"]
-    #saida_token(tbl_simbolos, count_linha, "saida_token.txt")
+    saida_token(tbl_simbolos, "saida_token.txt")
     saida_count_token(tbl_simbolos, "saida_count_token.txt")
 
 def saida_count_token(tbl_simbolos, nome_saida):
     tokens_count = tbl_simbolos['token'].value_counts()
     with open(nome_saida, 'w') as arq:
         arq.write("+----------------------------------+\n")
-        arq.write("|          Tabela de Tokens        |\n")
+        arq.write("|         Contagem de Tokens       |\n")
         arq.write("+----------------------------------+\n")
         arq.write("| Token".ljust(22))
         arq.write("| Quantidade ".ljust(10))
@@ -24,10 +24,26 @@ def saida_count_token(tbl_simbolos, nome_saida):
         arq.write("+----------------------------------+\n")
     
 
-def saida_token(tbl_simbolos, linhas, nome_saida):
-    dg_linha = len(str(linhas))
+def saida_token(tbl_simbolos, nome_saida):
     with open(nome_saida, 'w') as arq:
-        arq.write("Tabela de Tokens\n")
-        arq.write("Linha\tColuna\tToken\tLexema\n")
+        arq.write("+-----------------------------------------------------------------------+\n")
+        arq.write("|                            Tabela de Tokens                           |\n")
+        arq.write("+-----------------------------------------------------------------------+\n")
+        arq.write("| Linha".ljust(10))
+        arq.write("| Coluna".ljust(10))
+        arq.write("| Token".ljust(22))
+        arq.write("| Lexema".ljust(30))
+        arq.write("|\n")
+        arq.write("+-----------------------------------------------------------------------+\n")
+        for i in range(len(tbl_simbolos)):
+            arq.write("| " + str(tbl_simbolos["linha"][i]).ljust(8))
+            arq.write("| " + str(tbl_simbolos["coluna"][i]).ljust(8))
+            arq.write("| " + tbl_simbolos["token"][i].ljust(20))
+            if(tbl_simbolos["lexema"][i] != None):
+                arq.write("| " + tbl_simbolos["lexema"][i].ljust(28))
+            else:
+                arq.write("| " + " ".ljust(28))
+            arq.write("|\n")
+        arq.write("+-----------------------------------------------------------------------+\n")
         
 main("ex1.cic")
