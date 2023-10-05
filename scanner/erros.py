1ce26cc01922dbc8859f272639c15139b38e8c8e
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-05 00:48:35 -0300

    att estados de erro

--- scanner/erros.py
+++ scanner/erros.py
@@ -1,14 +1,14 @@
 def get_erro(estado_att, char):
     ERR_CADEIA = ["q1"]
-    ERR_NUM_FORMAT = ["q9"]
-    ERR_FLOAT_FORMAT = ["q13", "q14"]
-    ERR_CIEN_FORMAT = ["q10", "q11", "q12"]
-    ERR_ID_FORMAT = ["q24"]
+    ERR_NUM_FORMAT = ["q10"]
+    ERR_FLOAT_FORMAT = ["q14", "q15"]
+    ERR_CIEN_FORMAT = ["q11", "q12", "q13"]
+    ERR_ID_FORMAT = ["q25"]
     ERR_NAO_RESERV = ["q2", "q3", "TK_RESERVADA"]
-    ERR_MOEDA_FORMAT = ["q6", "q7", "q8"]
-    ERR_NAO_OPER = ["q15", "q16", "q4"]
+    ERR_MOEDA_FORMAT = ["q6", "q7", "q8", "q9"]
+    ERR_NAO_OPER = ["q16", "q17"]
     ERR_COMENT_FORMAT = ["q18", "q19"]
-    ERR_COMENT_ABERTO = ["q19", "q20", "q25", "q21"]
+    ERR_COMENT_ABERTO = ["q20", "q21", "q22"]
 
     disc_mensagem = {"ERR_CADEIA_FORMAT": "Cadeia mal formatada.",
                     "ERR_NUM_FORMAT": "Numero mal formatado.", 
@@ -47,7 +47,7 @@ def get_erro(estado_att, char):
         return disc_mensagem["ERR_LEXICO"]
 
 def check_erro_coment(estado_att):
-    estados_erro = ["q19", "q20", "q25", "q21"]
+    estados_erro = ["q20", "q21", "q22"]
     if estado_att in estados_erro:
         return True
     else:
@@ -64,5 +64,3 @@ def trat_erro_coment(arquivo, cursor):
     while(char != '\n'):
         char = arquivo.read(1)
         cursor["posicao"] += 1
-        
-get_erro("q0", 'P')
